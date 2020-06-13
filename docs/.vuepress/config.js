d2e897344e75f47dfcb8c212a5c60761fcf6a8d2
luojw <luojw@dist.com.cn>
2020-06-13 17:51:12 +0800

    更新目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,7 +42,7 @@ module.exports = {
           title: "GIS",
           collapsable: true,
           children: [
-            "ArcgisAPI","GISsystem","SuperMapWebGL","SuperMapIserver"
+            "OpenLayers","ArcgisAPI","GISsystem","SuperMapWebGL","SuperMapIserver"
           ]
         },
         {
